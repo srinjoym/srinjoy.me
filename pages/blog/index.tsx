9f8cea2d68bfbbf0d0a0137a959dd0562fa38d5b
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2020-03-24 01:10:11 -0700

    change case to fix routes

---
+++ pages/blog/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from "../../components/Hero"
+import Card from '../../components/Card'
+import CardCollectionSection from '../../components/CardCollectionSection'
+import Navigation from '../../components/Navigation'
+
+
+const Home = () => (
+  <div>
+    <Hero />
+    <CardCollectionSection />
+  </div>
+)
+
+export default Home
