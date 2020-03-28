215e44d43372cfdd3388eddcfa2f49536edd8cfd
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2020-03-28 02:18:17 -0700

    add more events to covid19

--- pages/covid19.tsx
+++ pages/covid19.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Container from "../components/Container"
 import ReactPlayer from 'react-player'
 import { Box, Text } from '@chakra-ui/core'
-import styled from "styled-components";
+import styled from "styled-components"
+import ReactMarkdown from 'react-markdown'
+import { TwitterTweetEmbed } from 'react-twitter-embed'
 
 const VisibilitySensor = require('react-visibility-sensor').default;
 
@@ -72,24 +74,47 @@ export const Event = ({ interval, children }) =>
     </div>
   </EventListItem>
 
-function Covid19({ videos }){
+export const Quote = ({children}) =>
+  <Text>"{children}"</Text>
+
+export const TweetEvent = ({data}) => (
+  <div>
+    <Event interval={data.date}>
+      <TwitterTweetEmbed tweetId={data.tweetId} />
+    </Event>
+  </div>
+)
+
+export const VideoEvent = ({video}) => (
+  <VisibilitySensor>
+    {({isVisible}) =>
+      <div>
+        <Event interval={video.date}>
+          {video.quotes.map(quote => (
+            <ReactMarkdown source={'"'+quote+'"'}/>
+          ))}
+
+          <Box my={10}>
+            <ReactPlayer playing={isVisible} url={video.url}/>
+          </Box>
+        </Event>
+      </div>
+    }
+  </VisibilitySensor>
+)
+
+function Covid19({ events }){
   return (
     <Container>
       <Timeline>
-      {videos.map(video => (
-        <VisibilitySensor>
-          {({isVisible}) =>
-            <div>
-              <Event interval={video.date}>
-                <Text>{video.quote}</Text>
-                <Box my={10}>
-                  <ReactPlayer playing={isVisible} url={video.url} style={{marginLeft: "auto", marginRight: "auto"}}/>
-                </Box>
-              </Event>
-            </div>
-          }
-        </VisibilitySensor>
-      ))}
+      {events.map(event => {
+        switch(event.type) {
+          case 'tweet':
+            return <TweetEvent data={event}/>
+          case 'video':
+            return <VideoEvent video={event}/>
+        }
+      })}
       </Timeline>
     </Container>
   )
@@ -102,36 +127,88 @@ export async function getStaticProps() {
 
   // By returning { props: posts }, the Blog component
   // will receive `posts` as a prop at build time
-  const videos = [
+  const events = [
     {
-      date: '2/19/2020',
-      quote: '',
-      url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=92&end=101',
+      date: '3/17/2020',
+      type: 'video',
+      quotes: [
+        'I\'ve always know this is a pandemic. I felt it was a pandemic long before it was called a pandemic',
+      ],
+      url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=245&end=261',
+    },
+    {
+      date: '3/15/2020',
+      type: 'video',
+      quotes: [
+        'Reporter: On a scale of 1-10, how would you rate your response to this crisis? President Trump: I\'d rate it a 10, I think we\'ve done a great job',
+      ],
+      url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=208&end=244',
+    },
+    {
+      date: '3/13/2020',
+      type: 'video',
+      quotes: [
+        'Trump shakes hands with multiple CEOs during Coronavirus briefing despite social distancing recommendations...'
+      ],
+      url: 'https://www.youtube.com/embed/H0wYtsouUi0?start=47&end=80'
+    },
+    {
+      date: '03/11/2020',
+      type: 'video',
+      quotes: [
+        'Trump bans all travel from Europe. However, adminstration later has to correct and say that ban does **not** apply to American residents and any trade and cargo.'
+      ],
+      url: 'https://www.youtube.com/embed/EhP2JMgAwuE?start=125&end=177'
+    },
+    {
+      date: '03/09/2020',
+      type: 'tweet',
+      tweetId: '1237027356314869761'
+    },
+    {
+      date: '2/28/2020',
+      type: 'video',
+      quotes: [
+        'Now the democrats are *politicizing* the Coronavirus.',
+        'How\'s President Trump doing? They (House Democrats) go oh not good not good (in a mocking tone). They have no clue, they don\'t have any clue',
+        'This is their new **hoax**',
+        'We did something that\'s been pretty amazing. We have 15 people (infected) in this massive country and because of the fact that we went early, we could\'ve had a lot more than that. We\'re doing great'
+      ],
+      url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=102&end=139',
+    },
+    {
+      date: '2/24/2020',
+      type: 'tweet',
+      tweetId: '1232058127740174339'
     },
     {
       date: '2/14/2020',
-      quote: '',
+      type: 'video',
+      quotes: ['We have a very small number of people in the country with it, it\s like around 12, many of them are getting better and some are fully recovered already so we\'re in very good shape'],
       url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=82&end=90',
     },
     {
       date: '2/10/2020',
-      quote: 'Looks like by April, you know when in theory when it gets a little warmer it miraculously goes away, hope that\'s true',
+      type: 'video',
+      quotes: ['Looks like by April, you know when in theory when it gets a little warmer it miraculously goes away, hope that\'s true'],
       url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=60&end=78',
     },
     {
       date: '1/30/2020',
-      quote: 'Coronavirus is the new thing that a lot of people are talking about, hopefully won\'t be as bad as some people think it could be. But we\'re working very closely with and a lot of other people and a lot of other countries and we think we have it very well under control.',
+      type: 'video',
+      quotes: ['Coronavirus is the new thing that a lot of people are talking about, hopefully won\'t be as bad as some people think it could be. But we\'re working very closely with and a lot of other people and a lot of other countries and we think we have it very well under control.'],
       url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=41&end=59',
     },
     {
       date: '1/22/2020',
-      quote: 'We have it totally under control. There\'s one person coming in from China and we have it under control',
+      type: 'video',
+      quotes: ['We have it totally under control. There\'s one person coming in from China and we have it under control'],
       url: 'https://www.youtube.com/embed/2eB_xCk5ABw?start=9&end=37', // Jan. 22 Trump Interview
     },
   ]
   return {
     props: {
-      videos
+      events
     }
   }
 }
