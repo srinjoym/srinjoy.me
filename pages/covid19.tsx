3b96af5563d18c28549c080371de4ab88dfb7b95
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2020-03-28 11:53:24 -0700

    start redesign of covid pages

--- pages/covid19.tsx
+++ pages/covid19.tsx
@@ -1,121 +1,90 @@
 import React from 'react'
 import Container from "../components/Container"
 import ReactPlayer from 'react-player'
-import { Box, Text } from '@chakra-ui/core'
+import { Box, Text, Flex,Heading, Link } from '@chakra-ui/core'
 import styled from "styled-components"
 import ReactMarkdown from 'react-markdown'
 import { TwitterTweetEmbed } from 'react-twitter-embed'
 
 const VisibilitySensor = require('react-visibility-sensor').default;
 
-const Timeline = styled.ul`
+
+const Line = styled.div`
   position: relative;
-  max-width: 95%;
-  list-style: none;
-  &:before {
-    background-color: black;
+  max-width: 1200px;
+  height: 50px;
+  margin: 0 auto;
+
+  ::after {
     content: '';
-    margin-left: -1px;
     position: absolute;
+    width: 4px;
+    background-color: gray;
     top: 0;
-    left: 2em;
-    width: 2px;
-    height: 100%;
+    bottom: 0;
+    left: 50%;
+    margin-left: -3px;
   }
 `
 
-const EventListItem = styled.li`
-  position: relative;
-
-  .icon {
-    transform: rotate(45deg);
-    background-color: black;
-    outline: 10px solid white;
-    display: block;
-    margin: 0.5em 0.5em 0.5em -0.5em;
-    position: absolute;
-    top: 0;
-    left: 2em;
-    width: 1em;
-    height: 1em;
-  }
-
-  .body {
-    padding: 2em 2em 0 2em;
-    position: relative;
-    top: -1.875em;
-    left: 2em;
-    width: 95%;
-    h3 {
-      font-size: 1.75em;
-    }
-    h4 {
-      font-size: 1.2em;
-      margin-bottom: 1.2em;
-    }
-  }
-
-  .date {
-    color: white;
-    background-color: black;
-    box-shadow: inset 0 0 0 0em #ef795a;
-    display: inline-block;
-    margin-bottom: 1.2em;
-    padding: 0.25em 1em 0.2em 1em;
+const TwitterCenteredContainer = styled.div`
+  .twitter-tweet {
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
   }
 `
-export const Event = ({ interval, children }) =>
-  <EventListItem>
-    <label className="icon"></label>
-    <div className="body">
-      <p className="date">{interval}</p>
-
-      {children}
-    </div>
-  </EventListItem>
-
-export const Quote = ({children}) =>
-  <Text>"{children}"</Text>
 
 export const TweetEvent = ({data}) => (
-  <div>
-    <Event interval={data.date}>
-      <TwitterTweetEmbed tweetId={data.tweetId} />
-    </Event>
-  </div>
+  <TwitterCenteredContainer>
+    <TwitterTweetEmbed tweetId={data.tweetId} style={{marginLeft: "auto", marginRight: "auto"}}/>
+    <Line />
+  </TwitterCenteredContainer>
 )
 
 export const VideoEvent = ({video}) => (
-  <VisibilitySensor>
-    {({isVisible}) =>
-      <div>
-        <Event interval={video.date}>
-          {video.quotes.map(quote => (
-            <ReactMarkdown source={'"'+quote+'"'}/>
-          ))}
+    <VisibilitySensor scrollCheck={true}>
+      {({isVisible}) =>
+        <div>
+          <Box position="relative" borderWidth="1px" rounded="lg" overflow="hidden" p={8} mx={8}>
+            <Flex align="center">
+              <Box m={4}>
+                <Heading my={2} size="xl">Title</Heading>
+                <Heading my={2} size="lg">{video.date}</Heading>
 
-          <Box my={10}>
-            <ReactPlayer playing={isVisible} url={video.url}/>
+                {video.quotes.map(quote => (
+                  <ReactMarkdown source={quote}/>
+                ))}
+              </Box>
+
+              <Box m={4} rounded="lg">
+                <ReactPlayer
+                  playing={isVisible}
+                  url={video.url}
+                />
+              </Box>
+            </Flex>
           </Box>
-        </Event>
-      </div>
-    }
+          <Line />
+        </div>
+      }
   </VisibilitySensor>
 )
 
 function Covid19({ events }){
   return (
     <Container>
-      <Timeline>
-      {events.map(event => {
-        switch(event.type) {
-          case 'tweet':
-            return <TweetEvent data={event}/>
-          case 'video':
-            return <VideoEvent video={event}/>
-        }
-      })}
-      </Timeline>
+      <Box py={8}>
+        {events.map(event => {
+          switch(event.type) {
+            case 'tweet':
+              return <TweetEvent data={event}/>
+            case 'video':
+              return <VideoEvent video={event}/>
+          }
+        })}
+      </Box>
     </Container>
   )
 }
