89dfff57254c01adf805087d3aec581af8b2fc8f
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2021-12-27 15:01:29 -0800

    add opengraph

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,13 +5,30 @@ import Footer from '../Footer'
 import Head from 'next/head'
 import { NextSeo } from 'next-seo'
 
-const Layout = ({children, title, headerOffset=true, ...rest}) => (
-  <Box {...rest}>
-    <NextSeo title={title} noindex={false} />
-    <Navigation offset={headerOffset} />
-      {children}
-    <Footer />
-  </Box>
-)
+const Layout = ({children, title, headerOffset=true, ...rest}) => {
+  const openGraph = {
+    url: 'https://srinjoy.me',
+    type: 'website',
+    title: 'srinjoy.me',
+    description: "Srinjoy Majumdar's personal website",
+    images: [
+      {
+        url: 'https://github.com/srinjoym/srinjoy.me/blob/master/public/img/srinjoy.me-og.png?raw=true',
+        width: 1200,
+        height: 630,
+        alt: 'Og Image Alt',
+      },
+    ]
+  };
+
+  return (
+    <Box {...rest}>
+      <NextSeo title={title} noindex={false} openGraph={openGraph} />
+      <Navigation offset={headerOffset} />
+        {children}
+      <Footer />
+    </Box>
+  )
+}
 
 export default Layout
