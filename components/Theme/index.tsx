c8311cf5a089754c46b587a1c6e6bd5f7c2f4cfe
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2022-01-15 19:12:36 -0800

    use true dark mode

--- components/Theme/index.tsx
+++ components/Theme/index.tsx
@@ -1,28 +1,19 @@
 import { extendTheme, ColorModeOptions, theme as defaultTheme } from "@chakra-ui/react";
-
-const config: ColorModeOptions = {
-  useSystemColorMode: true,
-}
+import { mode } from '@chakra-ui/theme-tools'
 
 export const theme = extendTheme({
   fonts: {
     heading: `"Inter", system-ui, sans-serif`,
-    body: `"Inter", system-ui, sans-serif`
+    body: `"Inter", system-ui, sans-serif`,
   },
-  config
-})
-
-export const colorConfig = theme => ({
-  light: {
-    color: theme.colors.gray[800],
-    bg: undefined,
-    borderColor: theme.colors.gray[200],
-    placeholderColor: theme.colors.gray[400],
+  config: {
+    useSystemColorMode: true,
   },
-  dark: {
-    color: theme.colors.whiteAlpha[900],
-    bg: theme.colors.gray[900],
-    borderColor: theme.colors.whiteAlpha[300],
-    placeholderColor: theme.colors.whiteAlpha[400],
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode('white', 'black')(props)
+      },
+    }),
   },
 })
