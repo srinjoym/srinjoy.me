30b0a1f6e55d6be879ecd2621b72bc5d5c8107e8
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2022-10-09 17:32:38 -0700

    rounded box

--- components/Scene/index.jsx
+++ components/Scene/index.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF, Bounds, PresentationControls } from '@react-three/drei'
+import { useGLTF, Bounds, PresentationControls, RoundedBox } from '@react-three/drei'
 import { LayerMaterial, Depth, Fresnel } from 'lamina'
 
 export const Scene = () => (
@@ -9,7 +9,7 @@ export const Scene = () => (
       global
       config={{ mass: 2, tension: 500 }}
       snap={{ mass: 4, tension: 1500 }}
-      rotation={[0, 0.3, 0]}
+      rotation={[0, 0, 0]}
       polar={[-Math.PI / 3, Math.PI / 3]}
       azimuth={[-Math.PI / 1.4, Math.PI / 2]}
     >
@@ -24,7 +24,7 @@ export const Scene = () => (
 
 function Cursor(props) {
   const ref = useRef()
-  const { nodes } = useGLTF('/cursor.glb')
+  // const { nodes } = useGLTF('/cursor.glb')
   const { gradient } = { gradient: 0.7 }
 
   // Animate gradient
@@ -38,7 +38,7 @@ function Cursor(props) {
   })
 
   return (
-    <mesh {...props} geometry={nodes.Cube.geometry}>
+    <RoundedBox>
       <LayerMaterial ref={ref} toneMapped={false}>
         <Depth colorA="#ff0080" colorB="black" alpha={1} mode="normal" near={0.5 * gradient} far={0.5} origin={[0, 0, 0]} />
         <Depth colorA="blue" colorB="#f7b955" alpha={1} mode="add" near={2 * gradient} far={2} origin={[0, 1, 1]} />
@@ -46,7 +46,7 @@ function Cursor(props) {
         <Depth colorA="white" colorB="red" alpha={1} mode="overlay" near={1.5 * gradient} far={1.5} origin={[1, -1, -1]} />
         <Fresnel mode="add" color="white" intensity={0.5} power={1.5} bias={0.05} />
       </LayerMaterial>
-    </mesh>
+    </RoundedBox>
   )
 }
 
