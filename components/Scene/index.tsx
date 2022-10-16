0d84def88379e41a88beb58d4128142bbe7cb12c
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2022-10-16 15:57:10 -0700

    mess around more with lighting

--- components/Scene/index.tsx
+++ components/Scene/index.tsx
@@ -1,3 +1,4 @@
+import { useColorMode } from '@chakra-ui/react'
 import { Backdrop, ContactShadows, OrbitControls, PresentationControls, Stage } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense, useRef } from 'react'
@@ -5,12 +6,13 @@ import Camera from "./camera"
 
 export const Scene = ({photoUrls}) => {
   const ref = useRef()
+  const {colorMode} = useColorMode()
 
   return (
   <Canvas dpr={[1, 2]} camera={{ fov: 50, zoom: 20 }} shadows>
-    <ambientLight intensity={1} />
-    <spotLight intensity={2} position={[0.2, 0.4, 0.2]} />
-    <pointLight intensity={1} position={[-0.4, -0.1, -0.4]} />
+    <ambientLight intensity={colorMode === "dark" ? 2:0.5} />
+    <spotLight penumbra={1} intensity={colorMode === "dark" ? 6:1} position={[0.2, 0.4, 0.2]} />
+    <pointLight intensity={colorMode === "dark"? 4:1} position={[-2, -0.5, -2]} />
     <Suspense>
       <Camera photoUrls={photoUrls} />
     </Suspense>
